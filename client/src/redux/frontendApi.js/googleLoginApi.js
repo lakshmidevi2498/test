ea5b209836b10b5ba02ba44bcec11de23adcb04d
lakshmidevi2498 <lakshmi@Actimize.in>
2024-11-20 16:41:34 +0530

    hmhm

--- client/src/redux/frontendApi.js/googleLoginApi.js
+++ client/src/redux/frontendApi.js/googleLoginApi.js
@@ -1,15 +1,14 @@
 import API from "../../API/API";
-const api = new API();
-const endPoints = "auth/google";
+const api = new API(); 
  
 export const googleLoginApi = () => {
   return new Promise((resolve, reject) => {
   
-    const authWindow = window.open("http://localhost:5050/auth/google", "_self");
+    const authWindow = window.open("https://test-3ahx.onrender.com/auth/google", "_self");
 
  
     window.addEventListener("message", (event) => {
-      if (event.origin !== "http://localhost:3000") return;  
+      if (event.origin !== "https://test-1-ekpw.onrender.com") return;  
 
       const { user, token, error } = event.data;
 
