2f2f147a1d57d014b43de36ba75c9854554c4802
lakshmidevi2498 <lakshmi@Actimize.in>
2024-11-20 15:16:53 +0530

    hello

--- server/passport.js
+++ server/passport.js
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Strategy as FacebookStrategy } from "passport-facebook";
-import userSchema from '../models/userSchemaModal.js';
+import userSchema from './models/userSchemaModal';
 import dotenv from 'dotenv';
 dotenv.config();
 
