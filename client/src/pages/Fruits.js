569f58a7e341f05636eebac40f1b18c20449ae78
lakshmidevi2498 <lakshmi@Actimize.in>
2024-11-20 14:14:40 +0530

    small changes

---
+++ client/src/pages/Fruits.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavbarComponent from '../components/NavbarComponent'
+
+const Fruits = () => {
+  return (
+    <>
+    <NavbarComponent/>
+     <div>Fruits</div>
+    </>
+   
+  )
+}
+
+export default Fruits
---
+++ client/src/pages/VegetablePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavbarComponent from '../components/NavbarComponent'
+
+const VegetablePage = () => {
+  return (
+    <>
+    <NavbarComponent/>
+    <div>VegetablePage</div>
+    </>
+    
+  )
+}
+
+export default VegetablePage
